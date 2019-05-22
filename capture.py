938c34cffd7cc233e0bb900f92bcdf51ad1188af
aatack <alexanderjtatack@gmail.com>
2019-05-22 01:56:04 +0100

    Add basic state capturing tools

---
+++ capture.py
@@ -0,0 +1,31 @@
+import pyautogui as pg
+import keyboard
+
+
+class Capture:
+    def capture(self):
+        """Capture some aspect of the current state of the computer."""
+        raise NotImplementedError()
+
+
+class ScreenRegion(Capture):
+    def __init__(self, x, y, w, h):
+        """Captures the pixels of the screen within a certain region."""
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+
+    def capture(self):
+        """Capture some aspect of the current state of the computer."""
+        return pg.screenshot(region=(self.x, self.y, self.w, self.h))
+
+
+class KeyState(Capture):
+    def __init__(self, key):
+        """Records whether or not a key is pressed."""
+        self.key = key
+
+    def capture(self):
+        """Capture some aspect of the current state of the computer."""
+        return keyboard.is_pressed(self.key)
